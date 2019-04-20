6801eea2c70d05ef754b8eb7399a97dfd12505ac
Milan Lachance <milan.lachance@polymtl.ca>
2019-04-20 18:57:03 -0400

    you can now choose the length of the password

--- password-generator.py
+++ password-generator.py
@@ -1,17 +1,39 @@
 import secrets
 import string
+import argparse
 import pyperclip
+
 
 alphanumeric = string.ascii_letters + string.digits
 allCharacters = alphanumeric + string.punctuation
-length = 20
+default_length = 20
 
-while True:
-    password = ''.join(secrets.choice(allCharacters) for x in range(length))
-    if(any(c.islower() for c in password)
-            and any(c.isupper() for c in password)
-            and any(c.isdigit() for c in password)
-            and any(c in string.punctuation for c in password)):
-        break
 
-pyperclip.copy(password)
+def generate_password(length):
+    while True:
+        password = ''.join(secrets.choice(allCharacters) for x in range(length))
+        if(any(c.islower() for c in password)
+                and any(c.isupper() for c in password)
+                and any(c.isdigit() for c in password)
+                and any(c in string.punctuation for c in password)):
+            break
+    return password
+
+def positive(length):
+    try:
+        i = int(length)
+    except ValueError:
+        raise argparse.ArgumentTypeError("length must be an integer")
+    if i < 1:
+        raise argparse.ArgumentTypeError("length must be at least 1")
+    return i
+
+
+parser = argparse.ArgumentParser(description="password generator")
+parser.add_argument("-l", "--length", type=positive, default=default_length, help="specify the length of the password")
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    password = generate_password(args.length)
+    pyperclip.copy(password)
