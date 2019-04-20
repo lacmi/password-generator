a6dd10f31486d13d8ad7b930ff6c88704a17b3ad
Milan Lachance <milan.lachance@polymtl.ca>
2019-04-20 19:37:24 -0400

    you can now print the password

--- password-generator.py
+++ password-generator.py
@@ -13,9 +13,9 @@ def generate_password(length, spl_chars):
         characters += string.punctuation
     while True:
         password = ''.join(secrets.choice(characters) for x in range(length))
-        if(any(c.islower() for c in password) and any(c.isupper() for c in password) and any(c.isdigit() for c in password)):
+        if (any(c.islower() for c in password) and any(c.isupper() for c in password) and any(c.isdigit() for c in password)):
             if spl_chars:
-                if(any(c in string.punctuation for c in password)):
+                if (any(c in string.punctuation for c in password)):
                     break
             else:
                 break
@@ -34,9 +34,13 @@ def fourplus(length):
 parser = argparse.ArgumentParser(description="password generator")
 parser.add_argument("-l", "--length", type=fourplus, default=default_length, help="specify the length of the password")
 parser.add_argument("-a", "--alphanumeric", action="store_false", help="only alphanumeric characters")
+parser.add_argument("-p", "--print", action="store_true", help="print the password instead of copying it to the clipboard")
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
     password = generate_password(args.length, args.alphanumeric)
-    pyperclip.copy(password)
+    if args.print:
+        print(password)
+    else:
+        pyperclip.copy(password)
